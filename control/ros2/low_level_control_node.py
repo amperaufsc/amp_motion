96f66edb4d57c496236d2a97944440ff33660bd3
lucassmoro <lucas.sm.souza@gmail.com>
2026-01-29 19:04:24 -0300

    fix: interpret can message correctly

--- control/ros2/low_level_control_node.py
+++ control/ros2/low_level_control_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 import can
-from can_bus1.can_reader import StateCanReader
+from can_classes.can_reader import StateCanReader
 from rclpy.node import Node
 from fs_msgs.msg import ControlCommand
 from signals.signal_control import SignalsController
@@ -15,20 +15,23 @@ class LowLevelControl(Node):
         self.kp = 20 
         self.ki = 10 
         self.kd = 10 
-        self.t  = 
-        self.Tt =  
+        self.t  = 1
+        self.Tt =  2
         self.limit = 90
         self.angle = 0.0
         self.angular_velocity = 0
+        self.measure = {}
         self.pid = PIDController(self.kp, self.ki, self.kd, self.t, self.Tt, self.limit, 100.0, -100.0)
         self.can = StateCanReader()
         self.get_logger().info("Funciona")
 
     def control_callback(self, control: ControlCommand.steering):
-        measure =self.can.can_reader["Estercamento_Atuador"]
-        pwm = self.pid.update_signal(control, measure)
-        self.pwm.steer(pwm)
-        self.get_logger().info("Repetição")
+        message = self.can.receive_message()
+        if message != None:
+            self.measure = self.can.can_reader(message)
+        #pwm = self.pid.update_signal(control, measure)
+        #self.pwm.steer(pwm)
+        self.get_logger().info(f"{self.measure}")
 
 def main(args=None):
     rclpy.init()
