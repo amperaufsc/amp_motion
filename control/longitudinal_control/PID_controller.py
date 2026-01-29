5b87578ce5ba4ae60b0839349d7848319757a9ca
lucassmoro <lucas.sm.souza@gmail.com>
2026-01-29 16:49:51 -0300

    feat: implement anti-windup to PID controller

--- control/longitudinal_control/PID_controller.py
+++ control/longitudinal_control/PID_controller.py
@@ -1,12 +1,15 @@
 class PIDController:
 
-    def __init__(self, Kp, Ki, Kd, T, max_signal=1.0, min_signal=-1.0):
+    def __init__(self, Kp, Ki, Kd, T, Tt, limit, max_signal=1.0, min_signal=-1.0):
         self.kp = Kp
         self.ki = Ki
         self.kd = Kd
         self.T = T
+        self.Tt = Tt
+        self.limit = limit
         self.D_ant = 0.0
         self.I_ant = 0.0
+        self.T_ant = 0.0
         self.erro_ant = 0.0
         self.max_signal = max_signal
         self.min_signal = min_signal
@@ -16,10 +19,14 @@ class PIDController:
         P = self.kp*erro
         I = self.I_ant + self.ki*self.T*(erro + self.erro_ant)
         D = self.D_ant + self.kd*((erro-self.erro_ant)/self.T)
+        T = -self.T_ant + self.Tt*self.T*(erro + self.erro_ant)
         sinal_controle = P + I + D
+        if sinal_controle - self.limit > 0:
+            sinal_controle += T
         self.erro_ant = erro
         self.I_ant = I
         self.D_ant = D
+        self.T_ant = T
 
         if sinal_controle > self.max_signal:
             sinal_controle = self.max_signal
