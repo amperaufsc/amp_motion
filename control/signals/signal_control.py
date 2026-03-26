fe3d82d667af7f5fd214d6a8c12f1392862e1611
lucassmoro <lucas.sm.souza@gmail.com>
2026-03-26 20:20:21 -0300

    test: actuator work without CAN

--- control/signals/signal_control.py
+++ control/signals/signal_control.py
@@ -1,19 +1,26 @@
 import RPi.GPIO as GPIO
 class SignalsController:
-    def __init__(self):
+    def __init__(self, left, right, PWM):
         GPIO.setmode(GPIO.BOARD)
-        self.pwm = GPIO.PWM(18, 100)
-        self.pwm.start(50)
-        self.left = 13
-        self.right = 15
+        self.pwm = GPIO.PWM(PWM, 100)
+        self.pwm.start(0)
+        self.left = left
+        self.right = right
         GPIO.setup(self.left, GPIO.OUT)
         GPIO.setup(self.right, GPIO.OUT)
+        self.x = 0
 
     def steer(self, control):
-        if control >= 0:
+        self.pwm.ChangeDutyCycle(abs(control))
+        if control > 0:
             GPIO.output(self.left, False)
-            GPIO.output(self.right, True)
-        elif control <= 0:
+            GPIO.output(self.right, True)  
+        elif control < 0:
             GPIO.output(self.left, True)
             GPIO.output(self.right, False)
-        self.pwm.ChangeDutyCycle(abs(control))
+        else:
+            GPIO.output(self.left, False)
+            GPIO.output(self.right, False)
+
+    def shutdown(self):
+        GPIO.cleanup()
