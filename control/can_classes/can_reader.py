61fa92b48e97ff81f03d7cd8cd458d5ed2998078
Lucas Dalmas <lucasdalmasrj@gmail.com>
2026-06-18 16:48:37 -0300

    fix:  fix incorrect dbc path

--- control/can_classes/can_reader.py
+++ control/can_classes/can_reader.py
@@ -9,7 +9,7 @@ from can_classes.can_proxy import CanBusCommunicator
 
 class StateCanReader():
     def __init__(self) -> None:
-        DBC_FILE = "src/amp_motion/control/can_classes/config/AS_CAN.dbc"
+        DBC_FILE = "src/amp_motion/control/config/AS_CAN.dbc"
         BUSTYPE = "socketcan"
         CHANNEL = "can0"
 
