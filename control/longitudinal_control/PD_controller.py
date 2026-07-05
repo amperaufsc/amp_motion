78ed3bd191d9821cd37d1ce3304ea954a19de0bf
Lucas Dalmas <lucasdalmasrj@gmail.com>
2026-07-05 00:24:53 -0300

    feat: add bias

--- control/longitudinal_control/PD_controller.py
+++ control/longitudinal_control/PD_controller.py
@@ -1,9 +1,10 @@
 class PDController:
 
-    def __init__(self, kp, kd, ts, max_signal=1.0, min_signal=-1.0):
+    def __init__(self, kp, kd, ts, bias, max_signal=1.0, min_signal=-1.0):
         self.kp = kp
         self.kd = kd
         self.ts = ts
+        self.bia = bias
         self.max_signal = max_signal
         self.min_signal = min_signal
         self.previous_measure = 0
@@ -14,7 +15,10 @@ class PDController:
         D = -self.kd*(measure - self.previous_measure)/self.ts
         self.previous_measure = measure
         sinal_controle = (P + D)
-
+        if sinal_controle > 0.5:
+            sinal_controle += self.bia
+        if sinal_controle < -0.5:
+            sinal_controle -= self.bia
         sinal_controle_limitado = max(self.min_signal, min(sinal_controle, self.max_signal) )
         
         return float(sinal_controle), float(sinal_controle_limitado), float(erro)
