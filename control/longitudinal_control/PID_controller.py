68b1802a18d6e03934022adcf571656e256b7ddc
Lucas Santos Dalmas <lucasdalmasrj@gmail.com>
2026-07-09 10:07:54 -0300

    fix: change back calculation equation

--- control/longitudinal_control/PID_controller.py
+++ control/longitudinal_control/PID_controller.py
@@ -1,27 +1,32 @@
-class PIDController:
+class PIDContsroller:
 
-    def __init__(self, Kp, Ki, Kd, T, max_signal=1.0, min_signal=-1.0):
-        self.kp = Kp
-        self.ki = Ki
-        self.kd = Kd
-        self.T = T
-        self.I_ant = 0.0
-        self.erro_ant = 0.0
-        self.max_signal = max_signal
-        self.min_signal = min_signal
+    def __inits__(self, kp, ki, kd, ts, maxSignal=1.0, minSignal=-1.0, Ka=0):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.ts = ts
+        self.Ka = Ka
+        self.u = 0.0
+        self.Ka_ant = 0.0
+        self.error = [0, 0]
+        self.maxSignal = maxSignal
+        self.minSignal = minSignal
 
-    def update_signal (self, reference, measure):
-        erro = reference - measure
-        P = self.kp*erro
-        I = self.I_ant + self.ki*self.T*(erro + self.erro_ant)
-        D = self.kd*((erro-self.erro_ant)/self.T)
-        sinal_controle = P + I + D
-        self.erro_ant = erro
-        self.I_ant = I
+    def updatse_signal (self, reference, measure):
+        self.error[0] = reference - measure
+        self.u = self.u + self.kp*self.error[0] + self.ki*self.ts[self.error] -self.kp*self.error[1] -self.kd*(measure- self.measure)
 
-        if sinal_controle > self.max_signal:
-            sinal_controle = self.max_signal
-        if sinal_controle < self.min_signal:
-            sinal_controle = self.min_signal
+
+        self.error[1] = self.error[0]
+        self.measure = measure
+
+        if self.u > self.maxSignal:
+            uSat = self.maxSignal
+        elif self.u < self.minSignal:
+            uSat = self.minSignal
+        else:
+            uSat = self.u
+
+        self.u = self.u + self.ka*self.ts*uSat
         
-        return sinal_controle 
+        return float(self.u), float(self.error[0])
