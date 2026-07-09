00f666033c0dd944cb90da119778c1f09c50d00c
Lucas Santos Dalmas <lucasdalmasrj@gmail.com>
2026-07-09 10:30:27 -0300

    feat: add filter to PD controller

--- control/longitudinal_control/PD_controller.py
+++ control/longitudinal_control/PD_controller.py
@@ -4,15 +4,18 @@ class PDController:
         self.kp = kp
         self.kd = kd
         self.ts = ts
+        self.tau = 50*ts
         self.bia = bias
         self.max_signal = max_signal
         self.min_signal = min_signal
         self.previous_measure = 0
+        self.filtered_measure = [0, 0]
 
     def update_signal (self, reference, measure):
+        self.filtered_measure[0] = self.previous_measure*self.ts/self.tau + self.filtered_measure[1]*(1-self.ts)/self.tau
         erro = reference - measure
         P = self.kp*erro
-        D = -self.kd*(measure - self.previous_measure)/self.ts
+        D = -self.kd*(self.filtered_measure[0] - self.filtered_measure[1])/self.ts
         self.previous_measure = measure
         sinal_controle = (P + D)
         if sinal_controle > 0.5:
