05d79f77ca246a639b53539a6ffa56d84dbcd9a3
Lucas Santos Dalmas <lucasdalmasrj@gmail.com>
2026-07-10 11:45:06 -0300

    feat: add bias option if ki = 0

--- control/longitudinal_control/PID_controller.py
+++ control/longitudinal_control/PID_controller.py
@@ -1,29 +1,33 @@
-class PIDContsroller:
+class PIDController:
 
-    def __inits__(self, kp, ki, kd, ts, maxSignal=1.0, minSignal=-1.0, Ka=0):
+    def __init__(self, kp, ki, kd, ts, maxSignal=1.0, minSignal=-1.0, ka=0.0, bias=0.0):
         self.kp = kp
         self.ki = ki
         self.kd = kd
         self.ts = ts
         self.tau = 50*ts
-        self.Ka = Ka
+        self.alpha = self.ts/(self.ts+self.tau)
+        self.ka = ka
         self.u = 0.0
-        self.error = [0, 0]
-        self.filtered_measure[0, 0]
+        self.error = [0.0, 0.0]
+        self.filteredMeasure = [0.0, 0.0]
         self.maxSignal = maxSignal
         self.minSignal = minSignal
+        self.bias = bias
 
-    def updatse_signal (self, reference, measure):
+        self.initialized = True
+
+    def update_signal (self, reference, measure):
+        self.measure = measure
+        if self.initialized == True:
+            self.initialized = False
+            self.filteredMeasure = [measure, measure]
         self.error[0] = reference - measure
 
-        self.filtered_measure[0] = self.measure*self.ts/self.tau + self.filtered_measure[1]*(1-self.ts)/self.tau
+        self.filteredMeasure[0] = self.measure*self.alpha + self.filteredMeasure[1]*(1-self.alpha)
 
-        self.u = self.u + self.kp*self.error[0] + self.ki*self.ts[self.error] -self.kp*self.error[1] -self.kd*(self.filtered_measure[0]- self.filtered_measure[1])
-
-
-        self.error[1] = self.error[0]
-        self.measure = measure
-
+        self.u = self.u + self.kp*(self.error[0] - self.error[1]) + self.ki*self.ts*self.error[0] -self.kd*(self.filteredMeasure[0]- self.filteredMeasure[1])/self.ts
+        
         if self.u > self.maxSignal:
             uSat = self.maxSignal
         elif self.u < self.minSignal:
@@ -31,6 +35,15 @@ class PIDContsroller:
         else:
             uSat = self.u
 
-        self.u = self.u + self.ka*self.ts*uSat
+        if self.ki != 0.0:
+            self.u = self.u + self.ka*self.ts*(uSat-self.u)
+        else:
+            if uSat > 0.5:
+                uSat += self.bias
+            elif uSat < -0.5:
+                uSat -= self.bias
+            uSat = max(self.minSignal, min(uSat, self.maxSignal))
         
-        return float(self.u), float(self.error[0])
+        self.error[1] = self.error[0]
+        self.filteredMeasure[1] = self.filteredMeasure[0]
+        return float(uSat), float(self.error[0])
