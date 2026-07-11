657e7a40b3d38f0382411b13520594e5722101f8
Lucas Santos Dalmas <lucasdalmasrj@gmail.com>
2026-07-10 21:52:23 -0300

    fix: change initialize value

--- control/longitudinal_control/PID_controller.py
+++ control/longitudinal_control/PID_controller.py
@@ -15,12 +15,12 @@ class PIDController:
         self.minSignal = minSignal
         self.bias = bias
 
-        self.initialized = True
+        self.initialized = False
 
     def update_signal (self, reference, measure):
         self.measure = measure
-        if self.initialized == True:
-            self.initialized = False
+        if self.initialized == False:
+            self.initialized = True
             self.filteredMeasure = [measure, measure]
         self.error[0] = reference - measure
 
@@ -28,21 +28,22 @@ class PIDController:
 
         self.u = self.u + self.kp*(self.error[0] - self.error[1]) + self.ki*self.ts*self.error[0] -self.kd*(self.filteredMeasure[0]- self.filteredMeasure[1])/self.ts
         
-        if self.u > self.maxSignal:
-            uSat = self.maxSignal
-        elif self.u < self.minSignal:
-            uSat = self.minSignal
-        else:
-            uSat = self.u
 
         if self.ki != 0.0:
-            self.u = self.u + self.ka*self.ts*(uSat-self.u)
+            if self.u > self.maxSignal:
+                uSat = self.maxSignal
+            elif self.u < self.minSignal:
+                uSat = self.minSignal
+            else:
+                uSat = self.u
+            uSat = uSat + self.ka*self.ts*(uSat-self.u)
         else:
-            if uSat > 0.5:
-                uSat += self.bias
-            elif uSat < -0.5:
-                uSat -= self.bias
-            uSat = max(self.minSignal, min(uSat, self.maxSignal))
+            if self.u > 0.5:
+                self.u += self.bias
+            elif self.u < -0.5:
+                self.u -= self.bias
+
+        uSat = max(self.minSignal, min(uSat, self.maxSignal))
         
         self.error[1] = self.error[0]
         self.filteredMeasure[1] = self.filteredMeasure[0]
